b568048ad5573488ce7d0af82c2639c21889533c
M9012593 <yogendrasahu1011@gmail.com>
2020-01-14 13:29:03 -0500

    form netlify

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,15 +4,29 @@ const About = () => (
   <Layout>
     <h1 className="text-2xl text-teal-400">About</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <form name="contact" netlify>
+    <form name="contact" method="POST" data-netlify="true">
       <p>
         <label>
-          Name <input type="text" name="name" />
+          Your Name: <input type="text" name="name" />
         </label>
       </p>
       <p>
         <label>
-          Email <input type="email" name="email" />
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Role:{" "}
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
         </label>
       </p>
       <p>
